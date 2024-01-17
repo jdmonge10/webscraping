b27ffbfd379d2919bc0fcb7db3b2c7e5ee574fff
jdmonge10 <jdmonge10@gmail.com>
2024-01-17 18:58:29 +0100

    proyecto_final

--- ejercicios/2.py
+++
@@ -1,9 +0,0 @@
-""". Modifique el script anterior para solicitar la edad del usuario y imprimir en pantalla la edad
-en meses. """
-
-nombre = input("Ingrese su nombre: ")
-edad = int(input("Ingrese su edad: "))
-
-edad_en_meses = edad * 12
-
-print(f"El nombre es {nombre}, su edad es {edad} años y en meses son {edad_en_meses}")
